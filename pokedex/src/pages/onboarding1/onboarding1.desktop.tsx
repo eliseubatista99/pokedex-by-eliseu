4e17e0f06fd36888bebb7b20bfbce78e5a655523
Eliseu Batista <eliseubatista99@outlook.com>
2023-04-08 19:12:33 +0100

    Onboarding 1 desktop

--- pokedex/src/pages/onboarding1/onboarding1.desktop.tsx
+++ pokedex/src/pages/onboarding1/onboarding1.desktop.tsx
@@ -1,9 +1,49 @@
+import { ImageAssets } from "../../assets";
+import {
+  CustomButton,
+  CustomImage,
+  Stepper,
+  Typography,
+} from "../../components";
 import { AppLayout } from "../../structure";
 
 export const Onboarding1DesktopScreen = () => {
   return (
     <AppLayout withoutNavigation>
-      <p>ONBOARDING 1 </p>
+      <CustomImage
+        src={ImageAssets.Onboarding1Figure}
+        alt={"A scientis and a boy with a straw hat"}
+        containerStyles={{
+          margin: "auto auto 37px auto",
+          maxWidth: "342px",
+          maxHeight: "265px",
+        }}
+      />
+      <div style={{ width: "100%", margin: "auto auto 0 auto" }}>
+        <Typography
+          appearance="headline_100"
+          containerProps={{ margin: "0 auto", textAlign: "center" }}
+        >
+          All Pokémon in One Place
+        </Typography>
+        <Typography
+          appearance="body_secondary"
+          containerProps={{ margin: "16px auto 0 auto", textAlign: "center" }}
+        >
+          Access a vast list of Pokémon from every generation ever made by
+          Nintendo
+        </Typography>
+        <Stepper
+          numberOfSteps={2}
+          currentStep={0}
+          containerProps={{ margin: "24px auto 0 auto" }}
+        />
+        <CustomButton
+          appearance={"primary"}
+          middleContent={"Continuar"}
+          containerProps={{ margin: "24px auto 120px auto" }}
+        />
+      </div>
     </AppLayout>
   );
 };
